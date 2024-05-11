ce3158e2b2a37da78cd5c36166c48767ae5c6c79
Arjun Dixit <arjun.dixit98@gmail.com>
2024-05-12 03:55:57 +0530

    push all

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+// const { REACT_APP_BACKEND_URL } = process.env;
 
 function App() {
   const [value, setValue] = useState("");
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get("http://localhost:8000/");
+      const { data } = await axios.get(process.env.REACT_APP_BACKEND_URL);
       console.log(data);
       const { name } = data;
       setValue(name);
     };
     getData();
   }, []);
+
   return <div className="App">Hello {value}</div>;
 }
 
